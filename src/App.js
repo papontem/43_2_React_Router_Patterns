13f424026b5098eaa7013c445c5aa9dcbaec96eb
papontemjetix <papontem@gmail.com>
2023-12-21 22:42:59 -0500

    addition of exercise provided mock data of 4 fake dogs

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import Routes from "./routes/Routes";
 import { BrowserRouter } from "react-router-dom";
 // import {} from "";
 
-function App() {
+function App(props) {
+  const {dogs} = props;
+  console.log("Dogs",dogs);
 	return (
 		<div>
 			<BrowserRouter>
@@ -18,6 +20,52 @@ function App() {
 			</BrowserRouter>
 		</div>
 	);
+}
+
+// Exercise provided default props
+App.defaultProps = {
+  dogs: [
+    {
+      name: "Whiskey",
+      age: 5,
+      src: "whiskey",
+      facts: [
+        "Whiskey loves eating popcorn.",
+        "Whiskey is a terrible guard dog.",
+        "Whiskey wants to cuddle with you!"
+      ]
+    },
+    {
+      name: "Duke",
+      age: 3,
+      src: "duke",
+      facts: [
+        "Duke believes that ball is life.",
+        "Duke likes snow.",
+        "Duke enjoys pawing other dogs."
+      ]
+    },
+    {
+      name: "Perry",
+      age: 4,
+      src: "perry",
+      facts: [
+        "Perry loves all humans.",
+        "Perry demolishes all snacks.",
+        "Perry hates the rain."
+      ]
+    },
+    {
+      name: "Tubby",
+      age: 4,
+      src: "tubby",
+      facts: [
+        "Tubby is really stupid.",
+        "Tubby does not like walks.",
+        "Angelina used to hate Tubby, but claims not to anymore."
+      ]
+    }
+  ]
 }
 
 export default App;
